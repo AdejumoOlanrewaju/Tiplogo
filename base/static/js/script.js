ab5b4c80777dbb1f3fa835326fde8dcd6b1ec9f1
AdejumoOlanrewaju <olanrewajuadejumo56@gmail.com>
2024-01-16 21:23:58 -0800

    setting up environment for the project

--- base/static/js/script.js
+++ base/static/js/script.js
@@ -8,16 +8,20 @@ let hamburgerSpan = document.querySelectorAll(".hamburger span")
 let navLinks = document.querySelectorAll("nav-links-parent a")
 let mobileMenu = document.querySelector(".mobile-menu")
 let closeMenu = document.querySelector(".close-menu")
+let tabLinks = document.querySelectorAll(".tab-links")
+let tabLine = document.querySelector(".tab-line")
 
 let counter = 0
 let touchStartX
 let touchEndX
 let swipeDifference
 let threshold = 50
-console.log(navLinks)
-console.log(scrollY)
+
+tabLine.style.width = `${tabLinks[0].offsetWidth}px`
+tabLine.style.left = `${tabLinks[0].offsetLeft}px`
+tabLine.style.opacity = "1"
+
 window.addEventListener("scroll", () => {
-    console.log(scrollY)
     if(scrollY > 0){
         document.querySelector("nav").classList.add("active")
         // hamburgerSpan.forEach(el => el.style.backgroundColor = "black")
@@ -28,6 +32,28 @@ window.addEventListener("scroll", () => {
     }
 })
 
+// Script for tab system in product page
+let width;
+let leftPosition
+tabLinks.forEach((link, index) => {
+    link.addEventListener("click", () => {
+        width = link.offsetWidth
+        leftPosition = link.offsetLeft
+        tabLine.style.width = `${link.offsetWidth}px`
+        tabLine.style.left = `${link.offsetLeft}px`
+        // tabLine.style.left = `${(index / tabLinks.length) * 100}%`
+        console.log(((index + 1) / tabLinks.length) * 100)
+        
+    })
+}) 
+
+// for(let [index, link] of tabLinks.entries()){
+//     console.log(index, link)
+// }
+
+/**
+ * (index/items.length) * 100%
+ */
 
 nextBtn.addEventListener("click", nextImage)
 function nextImage(){
