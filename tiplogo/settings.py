d5766618e2f0d56bf0d2cdde724f7541a5f34f4c
Buchi77344 <onyebuchifestus77@gmail.com>
2024-03-30 22:06:58 +0100

    hellofs

--- tiplogo/settings.py
+++ tiplogo/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+from .jazzmin import JAZZMIN_SETTINGS
 
 
 # Quick-start development settings - unsuitable for production
@@ -152,3 +153,4 @@ MEDIA_ROOT = BASE_DIR / "media"
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+JAZZMIN_SETTINGS =JAZZMIN_SETTINGS
